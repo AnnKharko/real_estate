e4be4d74f6d5042346e3e81452c8f6abd7a72da8
Ann <kharko2112.mechmat@gmail.com>
2021-07-02 13:43:18 +0300

    +

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,15 +2,14 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import { reducer } from './reducers';
-import { saveStoreToLocalStorage } from './middlewares'
 
 const persistedState = localStorage.getItem('state') ? JSON.parse(localStorage.getItem('state')) : {};
 
-export const store = createStore(reducer, persistedState, applyMiddleware(thunk, saveStoreToLocalStorage));
+export const store = createStore(reducer, persistedState, applyMiddleware(thunk));
 
 store.subscribe(() => {
     localStorage.setItem('state', JSON.stringify(store.getState()))
-})
+});
 export * from './action-types';
 export * from './action-creators';
 
--- src/redux/middlewares/saveStoreToLocalStorage.js
+++ src/redux/middlewares/saveStoreToLocalStorage.js
@@ -1,10 +1,10 @@
 export const saveStoreToLocalStorage = (store) => (next) => (action) => {
 
-    if (action.type === 'LOAD_STATE_FROM_LOCAL_STORAGE') {
-        if (localStorage.getItem('state') !== action.payload) {
-            localStorage.setItem('state', action.payload);
-        }
-    }
+    // if (action.type === 'LOAD_STATE_FROM_LOCAL_STORAGE') {
+    //     if (localStorage.getItem('state') !== action.payload) {
+    //         localStorage.setItem('state', action.payload);
+    //     }
+    // }
 
     return next(action);
 };
